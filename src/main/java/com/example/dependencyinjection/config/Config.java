75db8f860690c341275cd575a7559aecffbbb9c1
mursalbekov1 <mursalbekov.merey@gmail.com>
2024-02-08 23:06:15 +0600

    week3

--- src/main/java/com/example/dependencyinjection/config/Config.java
+++ src/main/java/com/example/dependencyinjection/config/Config.java
@@ -1,16 +1,22 @@
 package com.example.dependencyinjection.config;
 
-import com.example.dependencyinjection.model.BWPrinter;
 import com.example.dependencyinjection.model.ColorPrinter;
 import com.example.dependencyinjection.model.IPrintable;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class Config {
+
+    @Value("${server.port}")
+    private String port;
 
     @Bean
     IPrintable getPrinter() {
+        log.info(port);
         return new ColorPrinter();
     }
 }
--- src/main/java/com/example/dependencyinjection/controller/PrinterController.java
+++ src/main/java/com/example/dependencyinjection/controller/PrinterController.java
@@ -1,16 +1,17 @@
 package com.example.dependencyinjection.controller;
 
 import com.example.dependencyinjection.service.PrinterService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class PrinterController {
 
-    @Autowired
-    PrinterService printerService;
+    private final PrinterService printerService;
 
     @GetMapping("/print")
     public String Printer(@RequestParam(defaultValue = "Hello", value = "text") String text) {
